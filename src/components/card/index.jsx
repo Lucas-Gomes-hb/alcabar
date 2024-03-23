08826d419e93674e83a2ae88c83de44531d4b729
Lucas Gomes <lucas.gomes.pessoal@gmail.com>
2024-03-23 18:37:07 -0300

    cervejas e drinks

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,11 +1,87 @@
 import "./styles.css";
 
-export const Card = ({ title, body, image }) => {
+import { useState } from "react";
+import Modal from "react-modal";
+
+export const Card = ({ title, body, image, bodyStyle, hasModal }) => {
+  const classe = bodyStyle == null ? "" : bodyStyle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.4)",
+      zIndex: 100000,
+    },
+    content: {
+      width: "50%",
+      height: "80%",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundImage: `url(${image})`,
+      color: "#fff",
+      backgroundRepeat: "no-repeat",
+      padding: "0",
+      display: "grid",
+      alignContent: "space-between",
+      justifyItems: "end",
+    },
+  };
+
+  const openModal = () => {
+    if (hasModal == null) return;
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {};
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="card">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{body}</p>
-    </div>
+    <>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        portalClassName="modal"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="currentColor"
+          class="bi bi-x-circle"
+          viewBox="0 0 16 16"
+          className="modal-close"
+          onClick={closeModal}
+        >
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+        </svg>
+        <div className="modal-text">
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+      </Modal>
+      <div className={"card " + classe} onClick={openModal}>
+        <img src={image} alt={title} />
+        <div
+          style={{
+            display: "grid",
+            justifyItems: "center",
+          }}
+        >
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/drinkable/index.jsx
+++ src/components/drinkable/index.jsx
@@ -1,16 +1,184 @@
-import { Informacao } from "../informacao";
 import "./styles.css";
 
+import cerveja from "../../images/pilsen.jpg";
+import drink from "../../images/weiss.jpg";
+
+import { Card } from "../card";
+import { Informacao } from "../informacao";
+import { useEffect, useState } from "react";
+
 export const Drinkable = () => {
+  const [cards, setCard] = useState([]);
+  const [informations] = useState({
+    cervejas: [
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+      {
+        image: cerveja,
+        title: "Pilsen",
+        body: "Cerveja mais tradicional, mas não deixa de ter nosso toque especial.",
+      },
+    ],
+    drinks: [
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+      {
+        image: drink,
+        title: "Drink",
+        body: "Nada como um drink gelado para tomar enquanto aproveita sua festa.",
+      },
+    ],
+  });
+
+  const handleClickMenuOption = (e) => {
+    let activeDrink = document.querySelector(".activeDrink");
+    let activeBorder = document.querySelector(".activeDrink-border ");
+
+    if (activeDrink != null) activeDrink.classList = [];
+    if (activeBorder != null) activeBorder.classList = [];
+
+    if (e.target.parentElement.className !== "options") {
+      e.target.parentElement.classList = ["activeDrink"];
+      e.target.classList = ["activeDrink-border"];
+    }
+    if (e.target.parentElement.className === "options") {
+      e.target.classList = ["activeDrink"];
+      e.target.children[0].classList = ["activeDrink-border"];
+    }
+
+    handleShowPhotos(e.target.id);
+  };
+
+  const handleMouseEnterButton = (e) => {
+    if (e.target.parentElement.className === "activeDrink") return;
+    if (e.target.className === "activeDrink") return;
+    e.target.children[0].classList = ["animeDrink-border"];
+  };
+
+  const handleMouseOutButton = (e) => {
+    let animeTeste = document.querySelector(".animeDrink-border");
+
+    if (animeTeste != null) {
+      animeTeste.classList = [];
+    }
+  };
+
+  const handleShowPhotos = (id) => {
+    let inteiro = 0;
+    setCard(
+      informations[id].map((card) => (
+        <Card
+          key={(inteiro += 1)}
+          title={card.title}
+          body={card.body}
+          image={card.image}
+          bodyStyle={"bodyOn"}
+          hasModal={true}
+        />
+      ))
+    );
+  };
+
+  useEffect(() => {
+    let inteiro = 0;
+    setCard(
+      informations["cervejas"].map((card) => (
+        <Card
+          key={(inteiro += 1)}
+          title={card.title}
+          body={card.body}
+          image={card.image}
+          bodyStyle={"bodyOn"}
+          hasModal={true}
+        />
+      ))
+    );
+  }, [informations]);
+
   return (
-    <div className="drinkable">
-      <Informacao
-        title={"Saboreie nossas Cervejas e Drinks"}
-        subTitle={"Tome o melhor"}
-        text={
-          "Todo mundo tem um gosto diferente mesmo que não saiba e temos certeza que nossa variedade de cervejas vai te impressionar. Assim como nossos drinks irão explodir seu paladar."
-        }
-      />
-    </div>
+    <>
+      <div className="drinkable">
+        <Informacao
+          title={"Saboreie nossas Cervejas e Drinks"}
+          subTitle={"Tome o melhor"}
+          text={
+            "Todo mundo tem um gosto diferente mesmo que não saiba e temos certeza que nossa variedade de cervejas vai te impressionar. Assim como nossos drinks irão explodir seu paladar."
+          }
+        />
+
+        <div className="drinkable-choices">
+          <div className="options">
+            <button
+              id="cervejas"
+              className="activeDrink"
+              onClick={handleClickMenuOption}
+              onMouseEnter={handleMouseEnterButton}
+              onMouseOut={handleMouseOutButton}
+            >
+              Cervejas
+              <div id="cervejas" className="activeDrink-border"></div>
+            </button>
+            <button
+              id="drinks"
+              onClick={handleClickMenuOption}
+              onMouseEnter={handleMouseEnterButton}
+              onMouseOut={handleMouseOutButton}
+            >
+              Drinks
+              <div id="drinks"></div>
+            </button>
+          </div>
+          <div className="show" id="show-photos">
+            {cards}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
